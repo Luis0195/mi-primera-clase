f0aea69ac5623c2cf9b1c60f481accf09d1aaa46
Luis Martínez Alonso <luis0pk0rgmail.com>
2020-10-30 09:45:05 +0100

    Commit Inicial

---
+++ DesktopComputer.java
@@ -0,0 +1,60 @@
+public class DesktopComputer {
+    
+    // Nombre del procesador
+    private String processorName;
+    // Cantidad de RAM
+    private int ramMemoryCapacity;
+    // Tiene grafica dedicada?
+    private boolean haveGraphicCard;
+    
+    /**
+     * Crea la torre con los valores introducidos al crear el ordenador
+     */
+    public DesktopComputer(String cpuName, int amountOfRamMemory) {
+        processorName = cpuName;
+        ramMemoryCapacity = amountOfRamMemory;
+        haveGraphicCard = true;
+    }
+    
+    /**
+     * Cambiar procesador
+     */
+    public void changeCpu(String newCpu) {
+        processorName = newCpu;
+    }
+    
+    /**
+     * Cambiar RAM
+     */
+    public void changeRam(int newRamAmount) {
+        ramMemoryCapacity = newRamAmount;
+    }
+    
+    /**
+     * Quitar o poner grafica
+     */
+    public void quitOrPutGpu(boolean theresGpu) {
+        haveGraphicCard = theresGpu;
+    }
+    
+    /**
+     * Que procesador tengo
+     */
+    public String getCpuName() {
+        return processorName;
+    }
+    
+    /**
+     * Cuanta RAM tengo
+     */
+    public int getRamAmount() {
+        return ramMemoryCapacity;
+    }
+    
+    /**
+     * Tengo grafica?
+     */
+    public boolean getGraphicCard() {
+        return haveGraphicCard;
+    }
+}
